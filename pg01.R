055b88f72a4233edbbb84628bb05c344cfbb5531
majj <aidear@163.com>
2015-01-12 22:42:26 +0800

    pg & plot

--- pg01.R
+++ pg01.R
@@ -1,27 +1,45 @@
 
 library(RPostgreSQL)
 
+library(ggplot2)
+library(scales)
 
 query <- function(){
     drv <- dbDriver("PostgreSQL")
     con <- dbConnect(drv, user = "mabotech",password = "mabouser", port = 6432, dbname = "maboss")
     res <- dbSendQuery(con, statement = paste(
-                          "SELECT  station, state_start, state_stop, duration  from public.engine_test limit 10"))
+                          "SELECT  station, state_start, state_stop, duration  from public.engine_test where station = 'TestZone6_TC10' and state_start>'2010-12-20 19:35:00' limit 20"))
     # we now fetch the first 100 records from the resultSet into a data.frame
-    data1 <- fetch(res, n = 5)   
-    dim(data1)
+    data1 <- fetch(res, n = -1)   
+    
+    #print(dim(data1))
 
-    print(data1)
+    #print(data1)
 
-    dbHasCompleted(res)
+    #dbHasCompleted(res)
 
     # let's get all remaining records
-    data2 <- fetch(res, n = -1)
+   # data2 <- fetch(res, n = -1)
 
-    return(data2)
+    #return(data2)
+    return(data1)
 }
 
 
-v <- query()
+df1<- query()
 
-print(v)
+print(df1)
+
+ plt <- ggplot(data = df1   ) +
+    
+        geom_point(aes(x=state_start,y = log(duration)), color="red", shape=0) + 
+        geom_line( aes(x=state_start,y = log(duration)) , color="red")        
+        #geom_point(aes(x=state_stop,y = log(duration)), color="blue", shape=0) + 
+        #geom_line( aes(x=state_stop,y = log(duration)), color="blue" )
+        
+        
+        #    scale_x_datetime( format = "%H:%M:%S")
+
+
+
+ggsave("output/pg01.png", width=8, height=2)
